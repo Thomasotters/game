2d655771c1f68be8aae3b6ee90ab2488aba30777
Thomasotters <thomasotterstett@u.boisestate.edu>
2021-03-23 15:44:34 -0600

    Update game.js

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ const SUITS = ["sp","cl","he","di"]
 const VALUES = ["1", "2", "3","4", "5", "6","7", "8", "9","10", "J", "Q","K", "A"]
 
 //buttons and listeners
-Export default class Deck
+ class Deck
 {
   constructor (cards = newdeck)
   {
