d6855192a2960971ef931990edaf3768e7eba2d6
Thomasotters <thomasotterstett@u.boisestate.edu>
2021-03-26 02:47:27 -0600

    yez

--- game.js
+++ game.js
@@ -8,11 +8,8 @@ TODO:
 //game logic
 var playerHand = 0;
 var enemyHand = 0;
-var cardRange = 10;
-var cardBankLength = 100;
 var playerin = 0;
 var enemyin = 0;
-
 //display
 var playerHealthDisp = document.getElementById('playerHand');
 var enemyHealthDisp = document.getElementById('enemyHand');
@@ -33,10 +30,12 @@ playerin ++;
 if(playerin == 1)
 {
 document.getElementById("card1name").innerHTML = parseInt(Math.random()*10);
+document.getElementById("playerHand").innerHTML = "Player Hand = " + document.getElementById("card1name").innerHTML;
 }
 if(playerin == 2)
 {
 document.getElementById("card2name").innerHTML = parseInt(Math.random()*10);
+//document.getElementById("playerHand").innerHTML =
 }
 if(playerin == 3)
 {
@@ -46,10 +45,45 @@ if(playerin == 4)
 {
 document.getElementById("card4name").innerHTML = parseInt(Math.random()*10);
 }
+if(playerin == 5)
+{
+document.getElementById("card5name").innerHTML = parseInt(Math.random()*10);
+}
+if(playerin == 6)
+{
+document.getElementById("card6name").innerHTML = parseInt(Math.random()*10);
+}
+
 }
 function Stay ()
 {
 HitButton.disabled = true;
+for (enemyin = 0; enemyin < 7; enemyin++) {
+if(enemyin = 1)
+{
+  document.getElementById("carde1name").innerHTML = parseInt(Math.random()*10);
+}
+if(enemyin = 2 && enemyHand < 17)
+{
+  document.getElementById("carde2name").innerHTML = parseInt(Math.random()*10);
+}
+if(enemyin = 3 && enemyHand < 17)
+{
+  document.getElementById("carde3name").innerHTML = parseInt(Math.random()*10);
+}
+if(enemyin = 4 && enemyHand < 17)
+{
+  document.getElementById("carde4name").innerHTML = parseInt(Math.random()*10);
+}
+if(enemyin = 5 && enemyHand < 17)
+{
+  document.getElementById("carde5name").innerHTML = parseInt(Math.random()*10);
+}
+if(enemyin = 6 && enemyHand < 17)
+{
+  document.getElementById("carde6name").innerHTML = parseInt(Math.random()*10);
+}
+}
 }
 
 function Start()
@@ -65,18 +99,26 @@ function gameOver() {
     if (enemyHand > 21) {
         infoDisp.innerHTML = "Thats too many computer";
         return true;
+        StayButton.disabled = true;
+        HitButton.disabled = true;
     }
     if (playerHand > 21) {
         infoDisp.innerHTML = "Sorry you lost try again";
         return true;
+        StayButton.disabled = true;
+        HitButton.disabled = true;
     }
     if (playerHand < enemyHand && Stay == true) {
         infoDisp.innerHTML = "Sorry you lost try again";
         return true;
+        StayButton.disabled = true;
+        HitButton.disabled = true;
     }
     if (playerHand > enemyHand && Stay == true) {
         infoDisp.innerHTML = "Congrats on winning";
         return true;
+        StayButton.disabled = true;
+        HitButton.disabled = true;
     }
 
     return false;
